008d2633120f493ad339fcdab2f4c15404ba20c1
mthsalberti <you@example.com>
2020-01-09 16:05:15 -0300

    upload 09/01 16:05

--- js/form/fields/DateFieldLooden.js
+++ js/form/fields/DateFieldLooden.js
@@ -3,10 +3,11 @@ const DateFieldLooden  = function(){
     let params;
     function render(params){
         DateFieldLooden.params = params;
+        let isMobile = /Android|webOS|iPhone|iPad|iPod|pocket|psp|kindle|avantgo|blazer|midori|Tablet|Palm|maemo|plucker|phone|BlackBerry|symbian|IEMobile|mobile|ZuneWP7|Windows Phone|Opera Mini/i.test(navigator.userAgent) ? {type: "date", class: ""} : {type: "text", class: "datepicker"}
         DateFieldLooden.renderedTemplate = `
             <div class="${params.width}">
                 <label>${params.label}</label>
-                <input type="text" class="datepicker" placeholder="${params.placeholder}">
+                <input type="${isMobile.type}" class="${isMobile.class}" placeholder="${params.placeholder}">
             </div>
         `;
 
